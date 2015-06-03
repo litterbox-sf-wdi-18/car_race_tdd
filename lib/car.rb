81a853e2af80a86ce2958e25c7932989a79c582e
rycwilson <rycwilson@gmail.com>
2015-06-03 13:24:33 -0700

    solution

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,17 @@
 class Car
-  #write Car class code here
+
+  attr_accessor  :speed
+
+  def initialize
+    @speed = 0
+  end
+
+  def accelerate new_speed
+    if new_speed.between?(0, 100) && new_speed > @speed
+      @speed = new_speed
+    else
+      puts "Speed must be between 0 and 100 and greater than current speed"
+    end
+  end
+
 end
--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,26 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+
+  attr_accessor :cars
+
+  def initialize
+    @cars = []
+    car1 = Car.new
+    car1.speed = rand(0..100)
+    car2 = Car.new
+    car2.speed = rand(0..100)
+    @cars << car1 << car2
+  end
+
+  def winner
+    return @cars.first if @cars.first.speed > @cars.last.speed
+    @cars.last
+  end
+
+  def loser
+    return @cars.first if @cars.first.speed < @cars.last.speed
+    @cars.last
+  end
+
 end
